f69e1abb7c5227d3be6576774ad988116d62d738
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-21 22:46:14 +0100

    Before hook added for visiting URL.

--- src/TestPlan.spec.ts
+++ src/TestPlan.spec.ts
@@ -1,14 +1,29 @@
-import { test, expect, chromium, Browser } from '@playwright/test';
+import { test, expect, chromium, Browser, Page } from '@playwright/test';
 import { LandingPage } from './pages/landingPage';
 import { WhatsNew } from './pages/whatsNew';
 
+
 // To run with chromium and headed -> npx playwright test --project chromium --headed
 
-test('User is able to go to the website.', async ({ page }) => {
-  let landingPage = new LandingPage(page);
-  await landingPage.visitUrl();
-  await landingPage.goToSale();
-  let whatsNew = new WhatsNew(page);
-  await whatsNew.goToShopNewYoga();
-  await page.waitForTimeout(2000);
+
+
+test.describe('', () => {
+  let page: Page;
+  test.beforeAll(async ({browser}) => {
+    page = await browser.newPage();
+    let landingPage = new LandingPage(page);
+    await landingPage.visitUrl();
+  });
+
+  test('User is able to go to "Sale" page.', async () => {
+    let landingPage = new LandingPage(page);
+    await landingPage.goToSale();
+  });
+  
+  test('User is able to view new Yoga collection.', async () => {
+    let whatsNew = new WhatsNew(page);
+    await whatsNew.goToShopNewYoga();
+    await page.waitForTimeout(2000);
+  });
 });
+
