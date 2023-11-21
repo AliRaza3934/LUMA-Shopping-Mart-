bd1298a56b465b2e8bb437d35375370315dfe545
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-21 15:46:58 +0100

    What's new page WIP.

--- src/TestPlan.spec.ts
+++ src/TestPlan.spec.ts
@@ -1,7 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium, Browser } from '@playwright/test';
 import { LandingPage } from './pages/landingPage';
 
 test('User is able to go to the website.', async ({ page }) => {
   let landingPage = new LandingPage(page);
   await landingPage.visitUrl();
+  await landingPage.goToSale();
+  await page.waitForTimeout(2000);
 });
--- src/pages/landingPage.ts
+++ src/pages/landingPage.ts
@@ -6,6 +6,10 @@ export class LandingPage{
         await this.page.goto('https://magento.softwaretestingboard.com/');
     }
 
+    public async goToSale():Promise<void>{
+        await this.page.locator("//div[@class='section-item-content nav-sections-item-content']//span[contains(., 'What')]").click();
+    }
+
     constructor(public page: Page){
         this.page = page;
     }
