6c1daf037abf97001e66e79ede4fb723d8301355
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-21 17:39:30 +0100

    WIP.

--- src/TestPlan.spec.ts
+++ src/TestPlan.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect, chromium, Browser } from '@playwright/test';
 import { LandingPage } from './pages/landingPage';
 
+// To run with chromium and headed -> npx playwright test --project chromium --headed
+
 test('User is able to go to the website.', async ({ page }) => {
   let landingPage = new LandingPage(page);
   await landingPage.visitUrl();
--- src/pages/landingPage.ts
+++ src/pages/landingPage.ts
@@ -2,12 +2,18 @@ import { Page } from "@playwright/test";
 
 export class LandingPage{
 
+    landingPageLocators = []
+
     public async visitUrl():Promise<void>{
         await this.page.goto('https://magento.softwaretestingboard.com/');
     }
 
     public async goToSale():Promise<void>{
         await this.page.locator("//div[@class='section-item-content nav-sections-item-content']//span[contains(., 'What')]").click();
+    }
+
+    public async checkNewYogaAttire():Promise<void>{
+        
     }
 
     constructor(public page: Page){
